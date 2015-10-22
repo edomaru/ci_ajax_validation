48dd37b94f541cf48da2e279d0a8758deedeac97
edo masaru <masaruedo@yahoo.com>
2015-10-22 20:16:53 +0700

    Validation done

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -12,6 +12,23 @@ class Users extends CI_Controller {
 
 	public function save()
 	{
-		die("Save the data");
+		$data = array('success' => false, 'messages' => array());
+
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules("username", "Username", "trim|required");
+		$this->form_validation->set_rules("email", "Email", "trim|required|valid_email");
+		$this->form_validation->set_rules("password", "Password", "trim|required");
+		$this->form_validation->set_rules("password_confirm", "Password Confirm", "trim|required|matches[password]");
+		$this->form_validation->set_error_delimiters('<p class="text-danger">', '</p>');
+		if ($this->form_validation->run()) {
+			$data['success'] = true;
+		}
+		else {
+			foreach ($_POST as $key => $value) {
+				$data['messages'][$key] = form_error($key);
+			}
+		}
+
+		echo json_encode($data);
 	}
 }
--- application/views/users/create.php
+++ application/views/users/create.php
@@ -42,3 +42,55 @@
 		</div>
 	</div>
 </div>
+
+<script>
+	$('#form-user').submit(function(e) {
+		e.preventDefault();
+
+		var me = $(this);
+
+		// perform ajax
+		$.ajax({
+			url: me.attr('action'),
+			type: 'post',
+			data: me.serialize(),
+			dataType: 'json',
+			success: function(response) {
+				if (response.success == true) {
+					// if success we would show message
+					// and also remove the error class
+					$('#the-message').append('<div class="alert alert-success">' +
+						'<span class="glyphicon glyphicon-ok"></span>' +
+						' Data has been saved' +
+						'</div>');
+					$('.form-group').removeClass('has-error')
+									.removeClass('has-success');
+					$('.text-danger').remove();
+
+					// reset the form
+					me[0].reset();
+
+					// close the message after seconds
+					$('.alert-success').delay(500).show(10, function() {
+						$(this).delay(3000).hide(10, function() {
+							$(this).remove();
+						});
+					})
+				}
+				else {
+					$.each(response.messages, function(key, value) {
+						var element = $('#' + key);
+						
+						element.closest('div.form-group')
+						.removeClass('has-error')
+						.addClass(value.length > 0 ? 'has-error' : 'has-success')
+						.find('.text-danger')
+						.remove();
+						
+						element.after(value);
+					});
+				}
+			}
+		});
+	});
+</script>
